8522e984e4ca3e6885298c6dc4933cedaf405cf6
JahidHasan037 <jahid20cse037@gmail.com>
2024-09-24 01:03:42 +0800

    Something correction

---
+++ utilities.js
@@ -0,0 +1,38 @@
+
+
+
+
+function getInputFieldValueById(id)
+{
+
+
+    const InputValue=Number(document.getElementById(id).value);
+    document.getElementById(id).value='';
+    return InputValue;
+    
+    
+}
+
+
+
+function getInputFieldBeforNumberValueById(id)
+{
+
+
+    const InputValue=document.getElementById(id).value;
+   
+    return InputValue;
+    
+    
+}
+
+
+
+
+function getTextFieldValueById(id)
+{
+    const textValue=document.getElementById(id).innerText;
+    const textNumber=Number(textValue);
+    return textNumber;
+
+}
